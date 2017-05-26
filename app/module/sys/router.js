834874695a38ca7fd2e595c03558f020d7c26857
beiguanyi <bgyhnddr@163.com>
2017-05-26 15:51:37 +0800

    Signed-off-by: beiguanyi <bgyhnddr@163.com>

--- app/module/sys/router.js
+++ app/module/sys/router.js
@@ -1,9 +1,23 @@
 import Hello from '../sys/src/components/Hello'
 
-export default function(router) {
+export default (router) => {
   router.addRoutes([{
     path: '/',
     name: 'Hello',
-    component: Hello
+    component: Hello,
+    children: [{
+      path: 'demo',
+      component: Hello
+    }]
+  }])
+
+  router.addRoutes([{
+    path: '/',
+    name: 'Hello',
+    component: Hello,
+    children: [{
+      path: 'demo2',
+      component: Hello
+    }]
   }])
 }
--- build/dev-server.js
+++ build/dev-server.js
@@ -20,6 +20,7 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 // https://github.com/chimurai/http-proxy-middleware
 var proxyTable = config.dev.proxyTable
 
+require('./build-router')()
 var app = express()
 
 require('../app/entry')(app)
@@ -36,18 +37,22 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {}
 })
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+compiler.plugin('compilation', function(compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+    hotMiddleware.publish({
+      action: 'reload'
+    })
     cb()
   })
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
+Object.keys(proxyTable).forEach(function(context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {
+      target: options
+    }
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
